7ae68bb6553240d64c8b54c8613ee6f3eb5cf0ce
Elias <eliasseljehaug@hotmail.com>
2022-01-25 13:59:05 +0100

    25.01.2022
    Added new windows and Layout

--- python.py
+++ python.py
@@ -11,7 +11,7 @@ def logg_inn_win_layout():
                 [sg.Input(key='per_pass_inp')],
                 [sg.Text('Kort nummer:'), sg.Text(size=(15, 1), key='kort_nmr')],
                 [sg.Input(key='kort_nmr_inp')],
-                [sg.Button('Enter'), sg.Button('Cancel')],
+                [sg.Button('Enter'), sg.Button('Exit')],
                 [sg.Button('Create user')]
         ]
         return layout
@@ -27,7 +27,7 @@ def create_user_win_layout():
                 [sg.Input(key= 'password_inp')],
                 [sg.Text('confirm password', size=(15, 1), key='confirm_password')],
                 [sg.Input(key= 'confirm_password_inp')],
-                [sg.Button('create'), sg.Button('exit')]
+                [sg.Button('create'), sg.Button('Back')]
                 
         ]
         return layout
@@ -35,32 +35,62 @@ def create_user_win_layout():
 def one_time_code_win_layout():
         layout = [
                 [sg.Text('one time code', size=(15, 1), key='one_time_code_key')],
-                [sg.Input(key='one_time_code_inp')]
-                [sg.Button('Continue'),  sg.Button('Close')]
+                [sg.Input(key='one_time_code_inp')],
+                [sg.Button('Continue'),  sg.Button('Close')],
                 
 
         ]
 
         return layout
 
+
+def error_one_time_code_layout():
+        layout = [
+                [sg.Text('Error', size=(15, 1), key='error_one_time_code_key')],
+                [sg.Input(key='error_one_time_code_inp')],
+                [sg.Button('Close')],
+                
+
+        ]
+
+        return layout
+
+def bank_layout():
+        layout = [
+                [sg.Text('Bodø bank', size=(15, 1), key='bodø_bank_key')],
+                [sg.Input(key='bodø_bank_key')],
+                [sg.Button('Close')],
+                
+
+        ]
+
+        return layout
+
+
 logg_inn_win = sg.Window('logg_inn_win', logg_inn_win_layout(), element_justification='c') #finalize() and window.maximize() make the window full screan
 #window.maximize()
 create_user_win = sg.Window('create_user_win', create_user_win_layout(), element_justification='c') #finalize() and window.maximize() make the window full screan
 #window.maximize()
 one_time_code_win = sg.Window('one_time_code_win', one_time_code_win_layout(), element_justification='c') #finalize() and window.maximize() make the window full screan
-event3, event3 = one_time_code_win.read(timeout=100)
-event2, values2 = create_user_win.read(timeout=100)
-create_user_win.Hide()
-one_time_code_win.Hide()
 
+error_one_time_code_win = sg.Window('error_one_time_win', error_one_time_code_layout(), element_justification='c') #finalize() and window.maximize() make the window full screan
+
+#event2, values2 = create_user_win.read(timeout=100)
+event3, event3 = one_time_code_win.read(timeout=100)
+event4, event4 = error_one_time_code_win.read(timeout=100)
+
+#create_user_win.Hide()
+one_time_code_win.Hide()
+error_one_time_code_win.Hide()
 
 logg_inn_win_active = True
 create_user_win_active = False
 one_time_code_win_active = False
+error_one_time_code_active = False
 
 while True:  # Event Loop
         event, values = logg_inn_win.read(timeout=100)
-        if event in (None, 'Cancel'):
+        if event in (None, 'Exit'):
                 break
 
         
@@ -79,6 +109,8 @@ while True:  # Event Loop
                         
         if event == 'Create user':
                 create_user_win_active = True
+                event2, values2 = create_user_win.read(timeout=100)
+
                 create_user_win.UnHide()
                 logg_inn_win.Hide()
 
@@ -89,8 +121,8 @@ while True:  # Event Loop
         if create_user_win_active:
                 event2, values2 = create_user_win.read(timeout=100)
 
-                if event2 == 'exit':
-                        exit = True
+                if event2 == 'Back':
+                        Back = True
                       
                         logg_inn_win_active = True
                         logg_inn_win.UnHide()
@@ -105,7 +137,22 @@ while True:  # Event Loop
 
         if one_time_code_win_active:
                 event3, values3 = one_time_code_win.read(timeout=100)
+                 
+                if event3 == 'Exit':
+                        Cansel = True
+                        one_time_code_win.Hide()
+                        error_one_time_code_win.UnHide()
+                        
+        if error_one_time_code_active:
+                event4, values4 = error_one_time_code_win.read(timeout=100)
                 
+                if event == 'Close':
+                        error_one_time_code_win.Hide()
+                        logg_inn_win.UnHide()
+
+
+
+
                 
                                 
          
