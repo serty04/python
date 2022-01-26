4d90b850a206a19c815f6b3ca31d97cdb4fe78b8
Elias <eliasseljehaug@hotmail.com>
2022-01-26 11:10:35 +0100

    26.01.2022
    update

--- python.py
+++ python.py
@@ -8,9 +8,7 @@ def logg_inn_win_layout():
                 [sg.Text('Brukernavn:'), sg.Text(size=(15, 1), key = 'bruk_navn')],
                 [sg.Input(key='brukernavn_inp')],
                 [sg.Text('Personlig passord:'), sg.Text(size=(15, 1), key='per_pass')],
-                [sg.Input(key='per_pass_inp')],
-                [sg.Text('Kort nummer:'), sg.Text(size=(15, 1), key='kort_nmr')],
-                [sg.Input(key='kort_nmr_inp')],
+                [sg.Input(key='per_pass_inp', password_char='*')],
                 [sg.Button('Enter'), sg.Button('Exit')],
                 [sg.Button('Create user')]
         ]
@@ -32,61 +30,36 @@ def create_user_win_layout():
         ]
         return layout
 
-def one_time_code_win_layout():
-        layout = [
-                [sg.Text('one time code', size=(15, 1), key='one_time_code_key')],
-                [sg.Input(key='one_time_code_inp')],
-                [sg.Button('Continue'),  sg.Button('Close')],
-                
 
-        ]
-
-        return layout
-
-
-def error_one_time_code_layout():
-        layout = [
-                [sg.Text('Error', size=(15, 1), key='error_one_time_code_key')],
-                [sg.Input(key='error_one_time_code_inp')],
-                [sg.Button('Close')],
-                
-
-        ]
-
-        return layout
 
 def bank_layout():
         layout = [
-                [sg.Text('Bodø bank', size=(15, 1), key='bodø_bank_key')],
+                [sg.Text('Bodø bank',size=(160, 50), key='bodø_bank_key'), sg.Button('logg out')],
                 [sg.Input(key='bodø_bank_key')],
-                [sg.Button('Close')],
+                [sg.Text('konto_nmr'), sg.Text('hvilken_konto')],
+                [sg.Text(('konto navn'), key='konto_navn_tf')] [sg.Text(('penger_på_konto'), key='konto penger')],
+                [sg.Button('Enter'),sg.Button('Close')],
                 
-
         ]
 
         return layout
-
 
 logg_inn_win = sg.Window('logg_inn_win', logg_inn_win_layout(), element_justification='c') #finalize() and window.maximize() make the window full screan
 #window.maximize()
 create_user_win = sg.Window('create_user_win', create_user_win_layout(), element_justification='c') #finalize() and window.maximize() make the window full screan
 #window.maximize()
-one_time_code_win = sg.Window('one_time_code_win', one_time_code_win_layout(), element_justification='c') #finalize() and window.maximize() make the window full screan
-
-error_one_time_code_win = sg.Window('error_one_time_win', error_one_time_code_layout(), element_justification='c') #finalize() and window.maximize() make the window full screan
+bank_win = sg.Window('Bodø bank', bank_layout()) #finalize() and window.maximize,element_justification='c') #finalize() and window.maximize() make the window full screan
+#window.maximize()
 
 #event2, values2 = create_user_win.read(timeout=100)
-event3, event3 = one_time_code_win.read(timeout=100)
-event4, event4 = error_one_time_code_win.read(timeout=100)
+
 
 #create_user_win.Hide()
-one_time_code_win.Hide()
-error_one_time_code_win.Hide()
+
 
 logg_inn_win_active = True
 create_user_win_active = False
-one_time_code_win_active = False
-error_one_time_code_active = False
+bank_win_active = False
 
 while True:  # Event Loop
         event, values = logg_inn_win.read(timeout=100)
@@ -95,18 +68,15 @@ while True:  # Event Loop
 
         
         if event == 'Enter':
-                # Update the "output" text element to be the value of "input" element
-                logg_inn_win['bruk_navn'].update(values['brukernavn_inp'])
-                logg_inn_win['per_pass'].update(values['per_pass_inp'])
-
-                if event == 'Enter':
-                        one_time_code_win_active = True
-                        one_time_code_win.UnHide()
+                if values['brukernavn_inp'] == 'Elias' and values['per_pass_inp'] == '123':
+                        event3, values3 = bank_win.read(timeout=100)
+                        bank_win_active = True
                         logg_inn_win.Hide()
+                        bank_win.UnHide()
                         
-                       
-                       
-                        
+                else:
+                        sg.popup('Wrong username or password')
+        
         if event == 'Create user':
                 create_user_win_active = True
                 event2, values2 = create_user_win.read(timeout=100)
@@ -115,6 +85,13 @@ while True:  # Event Loop
                 logg_inn_win.Hide()
 
         
+        if bank_win_active:
+                event3, values3 = bank_win.read(timeout=100)
+                if event3 == 'Close':
+                        bank_active = False
+                        bank_win.close()
+                        
+
         
        
         
@@ -135,29 +112,4 @@ while True:  # Event Loop
                         logg_inn_win.UnHide()
                         create_user_win.Hide()
 
-        if one_time_code_win_active:
-                event3, values3 = one_time_code_win.read(timeout=100)
-                 
-                if event3 == 'Exit':
-                        Cansel = True
-                        one_time_code_win.Hide()
-                        error_one_time_code_win.UnHide()
-                        
-        if error_one_time_code_active:
-                event4, values4 = error_one_time_code_win.read(timeout=100)
-                
-                if event == 'Close':
-                        error_one_time_code_win.Hide()
-                        logg_inn_win.UnHide()
-
-
-
-
-                
-                                
-         
-
-
-                
-
-        
+       
